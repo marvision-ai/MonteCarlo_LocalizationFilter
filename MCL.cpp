0ac064d3cf31343a64bbb1c164b551ace5e6b28f
mbufi <mbufi1992@gmail.com>
2018-02-07 19:11:29 -0500

    motion and sensing

--- MCL.cpp
+++ MCL.cpp
@@ -224,6 +224,22 @@ void visualization(int n, Robot robot, int step, Robot p[], Robot pr[])
 */
 int main()
 {
-   
+    // Instantiate a robot object from the Robot class
+    Robot myrobot;
+
+    // Set robot new position to x=30.0, y=50.0 and orientation=PI/2
+    myrobot.set(30.0,50.0,M_PI/2.0);
+
+    // Turn clockwise by PI/2 and move by 15 meters
+    myrobot.move(-M_PI / 2.0, 15.0);
+
+    // Print the distance from the robot toward the eight landmarks
+    cout << myrobot.read_sensors()<<endl;
+
+    // Turn clockwise by PI/2 and move by 10 meters
+    myrobot.move(-M_PI / 2.0, 10.0);
+
+    // Print the distance from the robot toward the eight landmarks
+    cout << myrobot.read_sensors()<<endl;
     return 0;
 }
