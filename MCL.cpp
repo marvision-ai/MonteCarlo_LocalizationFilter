d70c50b53ac0f9b5e04f66bcac3f8722634ddca2
mbufi <mbufi1992@gmail.com>
2018-02-07 19:10:10 -0500

    helper functions

--- MCL.cpp
+++ MCL.cpp
@@ -152,8 +152,78 @@ private:
     }
 };
 
+// Functions
+double gen_real_random()
+{
+    // Generate real random between 0 and 1
+    uniform_real_distribution<double> real_dist(0.0, 1.0); //Real
+    return real_dist(gen);
+}
 
+double mod(double first_term, double second_term)
+{
+    // Compute the modulus
+    return first_term - (second_term)*floor(first_term / (second_term));
+}
+
+double evaluation(Robot r, Robot p[], int n)
+{
+    //Calculate the mean error of the system
+    double sum = 0.0;
+    for (int i = 0; i < n; i++) {
+        //the second part is because of world's cyclicity
+        double dx = mod((p[i].x - r.x + (world_size / 2.0)), world_size) - (world_size / 2.0);
+        double dy = mod((p[i].y - r.y + (world_size / 2.0)), world_size) - (world_size / 2.0);
+        double err = sqrt(pow(dx, 2) + pow(dy, 2));
+        sum += err;
+    }
+    return sum / n;
+}
+double max(double arr[], int n)
+{
+    // Identify the max element in an array
+    double max = 0;
+    for (int i = 0; i < n; i++) {
+        if (arr[i] > max)
+            max = arr[i];
+    }
+    return max;
+}
+/*
+void visualization(int n, Robot robot, int step, Robot p[], Robot pr[])
+{
+	//Draw the robot, landmarks, particles and resampled particles on a graph
+	
+    //Graph Format
+    plt::title("MCL, step " + to_string(step));
+    plt::xlim(0, 100);
+    plt::ylim(0, 100);
+
+    //Draw particles in green
+    for (int i = 0; i < n; i++) {
+        plt::plot({ p[i].x }, { p[i].y }, "go");
+    }
+
+    //Draw resampled particles in yellow
+    for (int i = 0; i < n; i++) {
+        plt::plot({ pr[i].x }, { pr[i].y }, "yo");
+    }
+
+    //Draw landmarks in red
+    for (int i = 0; i < sizeof(landmarks) / sizeof(landmarks[0]); i++) {
+        plt::plot({ landmarks[i][0] }, { landmarks[i][1] }, "ro");
+    }
+    
+    //Draw robot position in blue
+    plt::plot({ robot.x }, { robot.y }, "bo");
+
+	//Save the image and close the plot
+    plt::save("./Images/Step" + to_string(step) + ".png");
+    plt::clf();
+}
+*/
 int main()
-{ 
+{
+   
     return 0;
 }
