62f006f389cd5e93f726d2cae0a3b190efe7b56e
mbufi <mbufi1992@gmail.com>
2018-02-07 19:17:23 -0500

    simulating motion

--- MCL.cpp
+++ MCL.cpp
@@ -224,6 +224,7 @@ void visualization(int n, Robot robot, int step, Robot p[], Robot pr[])
 */
 int main()
 {
+    //Practice Interfacing with Robot Class
     Robot myrobot;
     myrobot.set_noise(5.0, 0.1, 5.0);
     myrobot.set(30.0, 50.0, M_PI / 2.0);
@@ -231,15 +232,29 @@ int main()
     //cout << myrobot.read_sensors() << endl;
     myrobot.move(-M_PI / 2.0, 10.0);
     //cout << myrobot.read_sensors() << endl;
-    
-    // Instantiating 1000 Particles each with a random position and orientation
+
+    // Create a set of particles
     int n = 1000;
-    Robot p[n]; 
-    
-    //For each particle add noise: Forward_Noise=0.05, Turn_Noise=0.05, and Sense_Noise=5.0
+    Robot p[n];
+
     for (int i = 0; i < n; i++) {
         p[i].set_noise(0.05, 0.05, 5.0);
-        cout << p[i].show_pose() << endl;
+        //cout << p[i].show_pose() << endl;
+    }
+
+    //Re-initialize myrobot object and Initialize a measurment vector
+    myrobot = Robot();
+    double* z;
+
+    //Move the robot and sense the environment afterwards
+    myrobot = myrobot.move(0.1, 5.0);
+    z = myrobot.sense();
+
+    // Simulate a robot motion for each of these particles
+    Robot p2[n];
+    for (int i = 0; i < n; i++) {
+        p2[i] = p[i].move(0.1, 5.0);
+        p[i] = p2[i];
     }
     return 0;
 }
